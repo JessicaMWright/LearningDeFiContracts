ec92a783a10f7c9ebb344bbeea1c3687166e990c
ZenoTheRuler <zeno@DESKTOP-FQKUIFM.localdomain>
2022-05-05 23:27:49 -0400

    defi contracts

--- contracts/defiContracts/CrowedFund.sol
+++ contracts/defiContracts/CrowedFund.sol
@@ -15,7 +15,12 @@ contract CrowdFund {
         uint endAt
     );
 
-    event Cancel(uint _id);
+    event Cancel(uint id);
+    event Pledge(uint indexed id, address indexed caller, uint _amount);
+    event UnPledge(uint indexed id, address indexed caller, uint _amount);
+    event Claimed(uint id);
+    event Refund(uint indexed id, address indexed caller, uint amount);
+
 
     struct Campaign {
         address creator;
@@ -23,7 +28,7 @@ contract CrowdFund {
         uint pledged;
         uint32 startAt;
         uint32 endAt;
-        bool claim;
+        bool claimed;
     }
 
     IERC20 public immutable token;
@@ -38,8 +43,8 @@ contract CrowdFund {
 
     function launch(
         uint _goal,
-        uint _startAt,
-        uint _endAt
+        uint32 _startAt,
+        uint32 _endAt
 
     ) external {
         require(_startAt >= block.timestamp, "start at < now");
@@ -61,7 +66,7 @@ contract CrowdFund {
     }
         // to cancel campign
     function cancel(uint _id) external {
-        Campaign memory campign = campaigns[_id];
+        Campaign memory campaign = campaigns[_id];
         require(msg.sender == campaign.creator,"not creator");
         require(block.timestamp < campaign.startAt, "started");
         delete campaigns[_id];
@@ -70,15 +75,55 @@ contract CrowdFund {
     }
     
         function pledge(uint _id, uint _amount) external {
+            Campaign storage campaign = campaigns[_id];
+            require(block.timestamp >= campaign.startAt, "not started");
+            require(block.timestamp <= campaign.endAt, "ended");
+
+            campaign.pledged += _amount;
+            pledgedAmount[_id][msg.sender] += _amount;
+            token.transferFrom(msg.sender, address(this), _amount);
+
+            emit Pledge(_id, msg.sender, _amount);
 
     }
     function unpledge(uint _id, uint _amount) external {
+        Campaign storage campaign = campaigns[_id];
+        require(block.timestamp <= campaign.endAt, "ended");
+
+        campaign.pledged -= _amount;
+        pledgedAmount[_id][msg.sender] -= _amount;
+        token.transfer(msg.sender, _amount);
+
+        emit UnPledge(_id, msg.sender, _amount);
 
     }
     function claim(uint _id) external {
+        Campaign storage campaign = campaigns[_id];
+        require(msg.sender == campaign.creator, "not creator");
+        require(block.timestamp > campaign.endAt, "not ended");
+        require(campaign.pledged >= campaign.goal, "pledged < goal");
+        require(!campaign.claimed, "claimed");
+
+        campaign.claimed = true;
+        token. transfer(msg.sender, campaign.pledged);
+
+        emit Claimed(_id);
+        
+
+
 
     }
     function refund(uint _id) external {
+        Campaign storage campaign = campaigns[_id];
+        require(block.timestamp > campaign.endAt, "not ended");
+        require(campaign.pledged < campaign.goal, "pledged < goal");
+
+        uint bal = pledgedAmount[_id][msg.sender];
+        pledgedAmount[_id][msg.sender] = 0;
+        token.transfer(msg.sender, bal);
+
+        emit Refund(_id, msg.sender, bal);
+
 
     }
 
