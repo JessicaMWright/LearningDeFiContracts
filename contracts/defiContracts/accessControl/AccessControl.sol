0fb9f5c0335151d64f641953bc255c0c852f9455
ZenoTheRuler <zeno@DESKTOP-FQKUIFM.localdomain>
2022-05-06 21:00:04 -0400

    defi contracts

--- contracts/defiContracts/accessControl/AccessControl.sol
+++ contracts/defiContracts/accessControl/AccessControl.sol
@@ -5,10 +5,10 @@ pragma solidity ^0.8.10;
 contract AccessControl {
 
     event GrantRole(bytes32 indexed role, address indexed account);
-    event   RevokeRole(bytes32 indexed role, address indexed account);
+    event RevokeRole(bytes32 indexed role, address indexed account);
 
     mapping(bytes32 => mapping(address => bool)) public roles;
-
+    // make public to get the hash
     bytes32 private constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
     bytes32 private constant USER = keccak256(abi.encodePacked("USER"));
 
@@ -18,16 +18,23 @@ contract AccessControl {
 
 
     }
+    constructor() {
+        _grantRole(ADMIN, msg.sender);
+    }
 
     function _grantRole(bytes32 _role, address _account) internal {
         roles[_role][_account] = true;
         emit GrantRole(_role, _account);
 
     }
-    function grantRole(bytes32 _role, address _account) external  {
+    function grantRole(bytes32 _role, address _account) external onlyRole(ADMIN) {
         _grantRole(_role, _account);
 
     }
+    function revokeRole(bytes32 _role, address _account) external onlyRole(ADMIN) {
+        roles[_role][_account] = false;
+        emit RevokeRole(_role, _account);
+        
 
 
 
